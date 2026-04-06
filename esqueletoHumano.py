5ac0d0d81e130536e7cb26b988c1288288707cd1
Marcelo Castelo <marcelocastelo16@gmail.com>
2026-04-06 14:43:18 -0300

    mudanças

--- esqueletoHumano.py
+++ esqueletoHumano.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
+
+print('----------------------Esqueleto Humano--------------------')
 df = pd.read_csv('csv/adult-human-skeleton.csv')
 print(df.head())
 print('--------------------------------------------------------')
@@ -7,3 +11,24 @@ print('--------------------------------------------------------')
 print(df.sort_values(by='fused_from', ascending=False).head())
 print('--------------------------------------------------------')
 print(df.query('region == "neck"'))
+
+print('----------------------Mamiferos--------------------')
+
+mamifero = pd.read_csv('csv/mammal-neck-bones.csv')
+print(mamifero.head())
+print('--------------------------------------------------------')
+print(mamifero.query('species == "giraffe"'))
+print('--------------------------------------------------------')
+print(mamifero.query('neck_vertebrae != 7'))
+print('--------------------------------------------------------')
+
+print('----------------------Passaros--------------------')
+
+passaro = pd.read_csv('csv/bird-neck-bones.csv')
+print(passaro.head())
+print('--------------------------------------------------------')
+print(passaro.query('neck_vertebrae == neck_vertebrae.max()'))
+contar_passaro = passaro['neck_vertebrae'].value_counts()
+contar_passaro = contar_passaro.sort_index()
+contar_passaro.plot.bar()
+plt.show()
--- esqueletoMamiferos.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-mamifero = pd.read_csv('csv/mammal-neck-bones.csv')
-print(mamifero.head())
-print('--------------------------------------------------------')
-print(mamifero.query('species == "giraffe"'))
-print('--------------------------------------------------------')
-print(mamifero.query('neck_vertebrae != 7'))
-print('--------------------------------------------------------')
